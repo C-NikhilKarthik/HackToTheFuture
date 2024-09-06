28066a72583c5c1738621ce6cc5b71ca70cd0f97
C Nikhil Karthik <nikhilkarthik241103@gmail.com>
2024-09-07 02:48:59 +0530

    sponsors added

--- components/Sponsors/index.tsx
+++ components/Sponsors/index.tsx
@@ -6,54 +6,106 @@ import { SponsorCardProps } from "./SponsorCard";
 const sponsorsTier1: SponsorCardProps[] = [
   {
     id: "sponsor-1-1",
-    name: "Amazon",
-    website: "https://www.amazon.com",
-    logo: "/amazonlogo.png",
+    name: "Revealing soon",
+    website: "/",
+    logo: "/Revealing.png",
     bgColor1: "#000",
     bgColor2: "#ccc",
   },
   {
     id: "sponsor-1-2",
-    name: "Amazon",
-    website: "https://www.amazon.com",
-    logo: "/amazonlogo.png",
+    name: "Revealing soon",
+    website: "/",
+    logo: "/Revealing.png",
     bgColor1: "#000",
     bgColor2: "#ccc",
   },
   {
     id: "sponsor-1-3",
-    name: "Amazon",
-    website: "https://www.amazon.com",
-    logo: "/amazonlogo.png",
+    name: "Revealing soon",
+    website: "/",
+    logo: "/Revealing.png",
     bgColor1: "#000",
     bgColor2: "#ccc",
   },
 ];
 
-const sponsorsTier2: SponsorCardProps[] = [
+const sponsorsTier2: SponsorCardProps[] = [];
+const sponsorsTier3: SponsorCardProps[] = [
   {
-    id: "sponsor-2-1",
-    name: "Devfolio",
-    website: "https://www.amazon.com",
-    logo: "/devfolio1.png",
-    bgColor1: "#fff",
-    bgColor2: "#fff",
-    fillOpacity: "1",
-    logoSize: "full",
+    id: "sponsor-1-1",
+    name: "DEVFOLIO LOGO",
+    website: "/",
+    logo: "/devfolio-image.png",
+    bgColor1: "#000",
+    bgColor2: "#ccc",
   },
   {
-    id: "sponsor-2-2",
-    name: "Devfolio",
-    website: "https://www.amazon.com",
-    logo: "/devfolio1.png",
-    bgColor1: "#fff",
-    bgColor2: "#fff",
-    fillOpacity: "1",
-    logoSize: "full",
+    id: "sponsor-1-2",
+    name: "BEECEPTOR LOGO",
+    website: "/",
+    logo: "/beeceptor.png",
+    bgColor1: "#000",
+    bgColor2: "#ccc",
+  },
+];
+const sponsorsTier4: SponsorCardProps[] = [
+  {
+    id: "sponsor-1-1",
+    name: "POLYGON LOGO",
+    website: "/",
+    logo: "/polygon.png",
+    bgColor1: "#000",
+    bgColor2: "#ccc",
+  },
+  {
+    id: "sponsor-1-2",
+    name: "ETHINDIA LOGO",
+    website: "/",
+    logo: "/ETHIndia.png",
+    bgColor1: "#000",
+    bgColor2: "#ccc",
+  },
+  {
+    id: "sponsor-1-3",
+    name: "GIVE MY CERTIFICATE LOGO",
+    website: "/",
+    logo: "/give-my-certificate.png",
+    bgColor1: "#000",
+    bgColor2: "#ccc",
+  },
+];
+const sponsorsTier5: SponsorCardProps[] = [
+  {
+    id: "sponsor-1-1",
+    name: "JETBRAINS LOGO",
+    website: "/",
+    logo: "/jetbrains.png",
+    bgColor1: "#000",
+    bgColor2: "#ccc",
   },
 ];
 
-const Sponsors: React.FC = () => (
+const sponsorsTier6: SponsorCardProps[] = [
+  {
+    id: "sponsor-1-1",
+    name: "GDG Hubli",
+    website: "/",
+    logo: "/gdg-hubli-logo-square-transparent.png",
+    bgColor1: "#fff",
+    bgColor2: "#fff",
+  },
+  {
+    id: "sponsor-1-2",
+    name: "IIIT Society",
+    website: "/",
+    logo: "/IIITians_Logo.png",
+    bgColor1: "#000",
+    bgColor2: "#ccc",
+  },
+];
+
+const SponsorsOriginal: React.FC = () => (
   <div
     id="sponsors"
     className="scroll-mt-12 text-white pt-16 pb-20 px-2 bg-[#070b0d] h-fit w-full flex flex-col items-center"
@@ -65,7 +117,7 @@ const Sponsors: React.FC = () => (
       </span>
     </h1>
     <div className="w-fit">
-      <div className="flex w-full gap-4 items-center mb-12 mt-8 max-w-4xl">
+      {/* <div className="flex w-full gap-4 items-center mb-12 mt-8 max-w-4xl">
         <div className="relative flex grow">
           <svg
             width={"10px"}
@@ -124,7 +176,7 @@ const Sponsors: React.FC = () => (
           </svg>
           <hr className="flex-grow border-t-2 border-teamBorderLine" />
         </div>
-      </div>
+      </div> */}
       <div className="flex gap-x-8 gap-y-8 justify-center flex-wrap w-full max-w-2xl items-center">
         {sponsorsTier2.map((sponsor, index) => (
           <SponsorCard key={index} {...sponsor} />
@@ -160,7 +212,7 @@ const Sponsors: React.FC = () => (
         </div>
       </div>
       <div className="flex gap-x-8 gap-y-8 justify-center flex-wrap w-full max-w-2xl items-center">
-        {sponsorsTier2.map((sponsor, index) => (
+        {sponsorsTier3.map((sponsor, index) => (
           <SponsorCard key={index} {...sponsor} />
         ))}
       </div>
@@ -194,7 +246,7 @@ const Sponsors: React.FC = () => (
         </div>
       </div>
       <div className="flex gap-x-8 gap-y-8 justify-center flex-wrap w-full max-w-2xl items-center">
-        {sponsorsTier2.map((sponsor, index) => (
+        {sponsorsTier4.map((sponsor, index) => (
           <SponsorCard key={index} {...sponsor} />
         ))}
       </div>
@@ -228,7 +280,41 @@ const Sponsors: React.FC = () => (
         </div>
       </div>
       <div className="flex gap-x-8 gap-y-8 justify-center flex-wrap w-full max-w-2xl items-center">
-        {sponsorsTier2.map((sponsor, index) => (
+        {sponsorsTier5.map((sponsor, index) => (
+          <SponsorCard key={index} {...sponsor} />
+        ))}
+      </div>
+
+      <div className="flex w-full gap-4 items-center mb-12 mt-12 max-w-4xl">
+        <div className="relative flex grow">
+          <svg
+            width={"10px"}
+            height={"10px"}
+            className="fill-teamBorderLine absolute top-[-4px] left-0 rotate-45"
+            viewBox="0 0 10 10"
+          >
+            <rect width={"10px"} height={"10px"} />
+          </svg>
+          <hr className="flex-grow border-t-2 border-teamBorderLine" />
+        </div>
+        <h1 className="text-xl md:text-2xl font-anton uppercase text-center">
+          Community Partners
+        </h1>
+
+        <div className="relative flex grow">
+          <svg
+            width={"10px"}
+            height={"10px"}
+            className="fill-teamBorderLine absolute top-[-4px] right-0 rotate-45"
+            viewBox="0 0 10 10"
+          >
+            <rect width={"10px"} height={"10px"} />
+          </svg>
+          <hr className="flex-grow border-t-2 border-teamBorderLine" />
+        </div>
+      </div>
+      <div className="flex gap-x-8 gap-y-8 justify-center flex-wrap w-full max-w-2xl items-center">
+        {sponsorsTier6.map((sponsor, index) => (
           <SponsorCard key={index} {...sponsor} />
         ))}
       </div>
@@ -236,4 +322,27 @@ const Sponsors: React.FC = () => (
   </div>
 );
 
-export default Sponsors;
+const Sponsors: React.FC = () => (
+  <div
+    id="sponsors"
+    className="scroll-mt-12 text-white pt-16 pb-20 px-2 bg-[#070b0d] h-fit w-full flex flex-col items-center"
+    data-idx="4"
+  >
+    <h1 className="text-[3rem] left-8 mt-28 mb-8 sm:left-20 w-full relative md:text-[90px] leading-[1] z-[2] uppercase font-anton">
+      Sponsors
+      <span className="absolute -left-2 md:-left-8 font-anton -bottom-4 z-[-1] text-[6rem] md:text-[200px] text-[#1b1e24]">
+        Sponsors
+      </span>
+    </h1>
+    <div className="grid place-items-center">
+      <h1 className="text-[3rem] left-8 mt-28 mb-8 sm:left-20 w-full relative md:text-[40px] leading-[1] z-[2] uppercase font-anton">
+        Revealing Soon
+        <span className="absolute min-w-fit text-nowrap -left-2 md:-left-8 font-anton -bottom-4 z-[-1] text-[6rem] md:text-[80px] text-[#1b1e24]">
+          Revealing Soon
+        </span>
+      </h1>
+    </div>
+  </div>
+);
+
+export default SponsorsOriginal;
